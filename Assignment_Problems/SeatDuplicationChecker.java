f3335cd748930c220720f3010d3fddfe3ac418a5
RohanAnand1705 <anandrohan1705@gmail.com>
2026-08-28 22:28:43 +0530

    add

---
+++ Assignment_Problems/SeatDuplicationChecker.java
@@ -0,0 +1,48 @@
+package Assignment_Problems;
+
+import java.util.Scanner;
+
+public class SeatDuplicationChecker {
+
+    static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            boolean duplicate = false;
+
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (duplicate) {
+                System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] seatNumbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter seat number: ");
+            seatNumbers[i] = sc.nextInt();
+        }
+
+        checkDuplicateSeats(seatNumbers);
+
+        sc.close();
+    }
+}
